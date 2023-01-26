5b639c9e79477fda10428acb31ee4d920a0d3a1d
Georgy Kcojev <kcojev@gmail.com>
2023-01-26 18:56:09 +0300

    fixed navbar on scrolling

--- app.js
+++ app.js
@@ -19,6 +19,18 @@ navToggle.addEventListener('click', function() {
     }
 })
 // ********** fixed navbar ************
+const navbar = document.getElementById('nav')
+const topLink = document.querySelector('.top-link')
+window.addEventListener('scroll', function() {
+    const scrollHeight = window.pageYOffset
+    const navHeight = nav.getBoundingClientRect().height
 
+    if(scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav')
+    }
+    else {
+        navbar.classList.remove('fixed-nav')
+    }
+})
 // ********** smooth scroll ************
 // select links
